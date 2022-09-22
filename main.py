ab2096b47d0bd7faed5d7237a78145d2b20e28cc
javier-abia <javiabia01@gmail.com>
2022-09-22 17:03:10 +0200

    structure change - first functional draft
    - main.py --> the search is now made directly to each of the 467 airports in Europe with its IATA code and multiple departure airports
    - get_airports --> new script made to get all europe airports IATA codes
    - destinations.txt --> new file where Europe IATA codes are saved
    - departures.txt --> new file where are saved desired airports departures

--- main.py
+++ main.py
@@ -9,17 +9,33 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 
-from src import movement
+
 from src import flight
 from src import flights_data
 
 
 
-###############################
-####### Google Flights ########
-###############################
+# We gather destinations data and put it in a list
+destinations_file = open('destinations.txt', 'r')
+destinations_string = destinations_file.read()
+destinations = destinations_string.split(',') 
+destinations_file.close()
 
-# Initialize web driver
+
+# We gather departures data and put it in a list
+departures_file = open('departures.txt', 'r')
+departures_string = departures_file.read()
+departures = departures_string.split(',')
+departures_file.close()
+
+
+
+
+###########################################################
+##################### Google Flights ######################
+###########################################################
+
+# Initialize driver
 options = Options()
 options.add_argument("start-maximized")
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
@@ -27,7 +43,7 @@ action = ActionChains(driver) # Define actions in driver
 
 
 url_google = 'https://www.google.com/'
-url_flights = 'https://www.google.com/travel/explore?q=Flights+to+Europe+from+OPO+in+the+next+6+months+round+trip&hl=en-GB'
+url_flights = f'https://www.google.com/travel/explore?q=Flights+to+Europe+from+{departures[0]}+in+the+next+6+months+round+trip&hl=en-GB'
 
 
 # First search google, just to reject all cookies, since we cant reject them when searching google flights directly
@@ -39,104 +55,22 @@ driver.get(url_flights)
 
 
 
+###########################################################
+####################### Departure #########################
+###########################################################
 
-############################
-######### Filters ##########
-############################
+# Initialize class
+fd = flights_data(driver, action)
 
-# Press filter button
-WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/c-wiz[2]/div/div[2]/div/c-wiz/div[2]/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/button/span'))).click()
-time.sleep(2) # Needed to work
-
-# Filter price, to minimun
-filter_drag = driver.find_element(By.XPATH, '/html/body/c-wiz[2]/div/div[2]/div/c-wiz/div[2]/div/div/div[1]/div[1]/div/div/div/div[2]/div[3]/div/div[2]/div/div[1]/section[4]/div/div[1]/div/div/div/div[2]/div/div/div/div[2]/div[2]')
-action.drag_and_drop_by_offset(filter_drag, -277, 0).perform()
-
-# Close filter dialogue
-driver.find_element(By.XPATH,'/html/body/c-wiz[2]/div/div[2]/div/c-wiz/div[2]/div/div/div[1]/div[1]/div/div/div/div[2]/div[3]/div/div[1]/div[2]/span/button').click()
-time.sleep(3)
+# Add departures
+fd.add_departure(departures)
 
 
-
-
-
-#########################################
-######### Movement accross map ##########
-#########################################
-mv = movement(driver,action)
-
-zoom_button = driver.find_element(By.XPATH, '/html/body/c-wiz[2]/div/div[2]/div/c-wiz/section/div/div[1]/div[1]/span/div/div/div[13]/div/div[2]/div/button[1]')
-mv.zoom(zoom_button, 3)
-
-# Focus on map to move with keys
-map_body = driver.find_element(By.XPATH, '/html/body/c-wiz[2]/div/div[2]/div/c-wiz/section/div/div[1]/div[1]/span/div/div/div[2]/div[2]')
-map_body.click()
-
-# Time to let map fully load
-time.sleep(1.5)
-
-# Movement to Azores islands
-mv.jump_left(4)
-mv.move_left(0.7)
-mv.jump_down(3)
-
-# We need to click and drag, so the web can reload flights in current location
-mv.reload_flights(map_body)
-
-
-
-
-###################################
-######### Data gathering ##########
-###################################
-
-# Initialize lists of atributes
-destination=[]; date=[]; price=[]; scales=[]; duration=[]
-atributes = [destination, date, price, scales, duration]
-
-# We need an empty list and the atributes XPath to define the data_gather class 
-atributes_empty = ['','','','','']
-atributes_path = ['//h3[@class="W6bZuc YMlIz"]','//div[@class="CQYfx"]','//span[@class="QB2Jof xLPuCe"]', '//span[@class="nx0jzf"]','//span[@class="Xq1DAb"]']
+###########################################################
+################### Data gathering ########################
+###########################################################
 
 # Initialize flights_data class and data saving
-fd = flights_data(driver, atributes_empty, atributes_path)
-fd.data_gather()
-
-
-
-
-# ####################################
-# ######### Sweep whole map ##########
-# ####################################
-
-# Spain
-time.sleep(1.5)
-mv.jump_right(2)
-mv.jump_up(1)
-mv.move_left(1)
-mv.move_down(1)
-time.sleep(0.2)
-
-mv.reload_flights(map_body)
-
-fd.data_gather()
-
-
-time.sleep(1.5)
-mv.move_up(1)
-mv.move_right(1)
-mv.jump_down(1)
-mv.jump_left(2)
-time.sleep(0.2)
-
-
-mv.reload_flights(map_body)
-time.sleep(2)
-
-fd.data_gather()
-
+fd.destination_gather(destinations)
 fd.show_flights()
 
-
-
-input()
--- src.py
+++ src.py
@@ -21,156 +21,14 @@ class flight:
 
 class flights_data:
 
-    def __init__(self, driver, action, data_empty_array, atb_path):
-        self.st = set()
-        self.driver = driver
-        self.action = action
-        self.data = data_empty_array # Empty array of data of concrete length
-        self.path = atb_path # Path to atributes
-        self.ignored_exceptions=(NoSuchElementException,StaleElementReferenceException)
-        self.search_box = '/html/body/c-wiz[2]/div/div[2]/div/c-wiz/div[2]/div/div/div[1]/div[1]/section/div/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/div/div[1]/div/div/input'
-
-
-
-    def data_gather(self, destinations):
-        '''
-        Gathers the data from google flights in a non-legible format.
-        ---------------------------------------------------------------
-        Translation non-legible format to text.
-        Saves all data in list of lists with following format---> 
-        ---> [[destinations], [dates], [prices], [scales], [duration]]
-        ---------------------------------------------------------------
-        Saves translated data to a set. Each element of the set is a flight
-        with all of its atributes.
-        '''
-        atb_data = [[],[],[],[],[]]
-
-        for i in range(len(self.path)):
-            time.sleep(2)
-            self.data[i] = self.driver.find_elements(By.XPATH, f'{self.path[i]}')
-            # self.data[i] = WebDriverWait(self.driver,5,ignored_exceptions=self.ignored_exceptions).until(EC.presence_of_all_elements_located((By.XPATH, f'{self.path[i]}')))
-            
-            print('len data', len(self.data))
-            print('len data[0]', len(self.data[0]))
-        for i in range(len(self.data)):
-            for j in range(len(self.data[i])):
-                if self.data[i][j].text != '':
-                    atb_data[i].append(self.data[i][j].text)
-        for  i in range(len(atb_data[0])): # data[0] = number of flights
-            f = flight(atb_data[0][i], atb_data[1][i], atb_data[2][i], atb_data[3][i], atb_data[4][i])
-            self.st.add(f)
-                            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    def show_flights(self):
-        ''' Prints flights '''
-        print('Destination \t - \t Date \t - \t Price \t - \t Scales \t - \t Duration')
-        for i in self.st:
-            print(f'--> {i.destination} \t - \t {i.date} \t - \t {i.price} \t - \t {i.scales} \t - \t {i.duration}')
-
-
-
-
-
-class movement:
-    def __init__(self,driver,action):
-        self.driver = driver
-        self.action = action
-        
-
-    def zoom(self,button,n):
-        for i in range(n):
-            button.click()
-
-
-    def move_left(self,t):
-        endtime = time.time() + t
-        
-        while True:
-            self.action.key_down(Keys.ARROW_LEFT).perform()
-        
-            if time.time() > endtime:
-                self.action.key_up(Keys.ARROW_LEFT).perform()
-                time.sleep(0.1)
-                break    
-
-    def move_right(self,t):
-        endtime = time.time() + t
-        
-        while True:
-            self.action.key_down(Keys.ARROW_RIGHT).perform()
-        
-            if time.time() > endtime:
-                self.action.key_up(Keys.ARROW_RIGHT).perform()
-                time.sleep(0.1)
-                break    
-
-    def move_up(self,t):
-        endtime = time.time() + t
-        
-        while True:
-            self.action.key_down(Keys.ARROW_UP).perform()
-        
-            if time.time() > endtime:
-                self.action.key_up(Keys.ARROW_UP).perform()
-                time.sleep(0.1)
-                break    
-
-    def move_down(self,t):
-        endtime = time.time() + t
-        
-        while True:
-            self.action.key_down(Keys.ARROW_DOWN).perform()
-        
-            if time.time() > endtime:
-                self.action.key_up(Keys.ARROW_DOWN).perform()
-                time.sleep(0.1)
-                break    
-
-    def jump_left(self,n):
-        for i in range(n):
-            self.action.send_keys(Keys.HOME)
-        time.sleep(0.1)
-
-    def jump_right(self,n):
-        for i in range(n):
-            self.action.send_keys(Keys.END)
-        time.sleep(0.1)
-
-    def jump_down(self,n):
-        for i in range(n):
-            self.action.send_keys(Keys.PAGE_DOWN)
-        time.sleep(0.1)
-
-    def jump_up(self,n):
-        for i in range(n):
-            self.action.send_keys(Keys.PAGE_UP)
-        time.sleep(0.1)
-
-
-    def reload_flights(self,map_body):
-        self.action.drag_and_drop_by_offset(map_body, -277, 0).perform()
-
-
-
-class search:
     def __init__(self, driver, action):
         self.driver = driver
         self.action = action
+        self.flights_list = []
+        self.data = ['','','','',''] # Empty array of data of concrete length
+        self.path = ['//span[@class="HVJNrc CIydMe"]','//div[@class="CQYfx c2y3C"]','//div[@class="EDKFBb QB2Jof SgNiff"]', '//span[@class="yApPxd"]'] # Path to atributes
         self.search_box = '/html/body/c-wiz[2]/div/div[2]/div/c-wiz/div[2]/div/div/div[1]/div[1]/section/div/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/div/div[1]/div/div/input'
+        self.ignored_exceptions=(NoSuchElementException,StaleElementReferenceException)
 
 
 
@@ -194,9 +52,22 @@ class search:
         map_body.click()
             
 
-    def search_destinations(self, destinations):
+        
+    def destination_gather(self, destinations):
 
-        for i in range(2):
+        '''
+        Gathers the data from google flights in a non-legible format.
+        ---------------------------------------------------------------
+        Translation non-legible format to text.
+        Saves all data in list of lists with following format---> 
+        ---> [[destinations], [dates], [prices], [scales], [duration]]
+        ---------------------------------------------------------------
+        Saves translated data to a set. Each element of the set is a flight
+        with all of its atributes.
+        '''
+
+
+        for i in range(len(destinations)):
             time.sleep(1)
             self.driver.find_element(By.XPATH, self.search_box).click()     
             self.action.send_keys(destinations[i]).perform() 
@@ -204,14 +75,30 @@ class search:
             self.action.key_up(Keys.ENTER).perform() 
     
 
-            # flight = int(WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH, fpath))).text.strip('€'))
-            time.sleep(3)
+            print(f'Iteration: {i}/{len(destinations)}')
             
             try:
-                price = int(self.driver.find_element(By.XPATH, '//div[@class="EDKFBb QB2Jof SgNiff"]').text.strip('€'))
-                    
+                price = int(WebDriverWait(self.driver,3).until(EC.element_to_be_clickable((By.XPATH, '//div[@class="EDKFBb QB2Jof SgNiff"]'))).text.strip('€'))
+                # price = int(self.driver.find_element(By.XPATH, '//div[@class="EDKFBb QB2Jof SgNiff"]').text.strip('€'))
+                if price < 100:
+                    for i in range(len(self.path)):
+                        self.data[i] = self.driver.find_element(By.XPATH, f'{self.path[i]}')
+                f = flight(self.data[0].text, self.data[1].text, self.data[2].text, self.data[3].text.split('\n')[0], self.data[3].text.split('\n')[1])
+                self.flights_list.append(f)
+            except:
+                pass
+
+            try:
+                # self.driver.find_element(By.XPATH, '/html/body/c-wiz[2]/div/div[2]/div/c-wiz/div[2]/div/div/div[2]/div/div[2]/div[1]/button/div[1]').click()                            
+                WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/c-wiz[2]/div/div[2]/div/c-wiz/div[2]/div/div/div[2]/div/div[2]/div[1]/button/div[1]'))).click()
             except:
                 pass
 
 
-            self.driver.find_element(By.XPATH, '/html/body/c-wiz[2]/div/div[2]/div/c-wiz/div[2]/div/div/div[2]/div/div[2]/div[1]/button/div[1]').click()
+    def show_flights(self):
+        ''' Prints flights '''
+        for i in self.flights_list:
+            print(f'--> {i.destination} \t - \t {i.date} \t - \t {i.price} \t - \t {i.scales} \t - \t {i.duration}')
+
+
+
