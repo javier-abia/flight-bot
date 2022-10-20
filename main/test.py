8065528f39ad4a8669a87443ae65eb6c11a43106
javier-abia <javiabia01@gmail.com>
2022-10-20 10:04:33 +0200

    first json-swapping test
    changing all test saving formats from txt to json. first draft testing
    json module.

--- main/test.py
+++ main/test.py
@@ -1,12 +1,13 @@
 from flights import flight
 from flights import flights_data
-import pickle
+import json
 
+f1 = flight('departure', 'IATA', 'destination', 'date', 'price', 'scales', 'duration')
+f2 = flight('departure1', 'IATA1', 'destination1', 'date1', 'price1', 'scales1', 'duration1')
+l = [f1,f2]
 
+with open('test.json', 'w') as fl:
+    for flight in l:
+        json.dump(flight.__dict__, fl, indent=2)
 
-
-with open('./docs/show/old_flights.txt', 'rb') as f:
-    data = pickle.load(f)
-
-fd = flights_data('asd', 'asd')
-fd.show_flights(data)
+        
